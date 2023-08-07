b9c6563674f7fdb3a24486d3784d92216fd746da
unknown <toretovoid@gmail.com>
2023-08-06 21:48:55 -0400

    fix error

--- src/main/java/com/example/user/controller/UserController.java
+++ src/main/java/com/example/user/controller/UserController.java
@@ -3,32 +3,30 @@ package com.example.user.controller;
 import com.example.user.model.user.dto.User;
 import com.example.user.model.user.request.UserRequest;
 import com.example.user.model.user.response.UserResponse;
-import com.example.user.services.UserServices;
+import com.example.user.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
-import java.awt.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
 public class UserController {
-    private final UserServices userServices;
+    private final UserService userService;
+
     @GetMapping(value = "/saveUser", consumes = (MediaType.APPLICATION_JSON_VALUE))
     public UserResponse saveUser(@RequestBody UserRequest userRequest) {
         User user = new User();
-        user.setUserName(userRequest.getName());
-        userServices.saveUser(user);
+        user.setUsername(userRequest.getName());
+        userService.createUser(user);
         UserResponse userResponse = new UserResponse();
-        userResponse.setName(user.getUserName());
+        userResponse.setName(user.getUsername());
         userResponse.setLastname(userRequest.getLastname());
         userResponse.setEmail(userRequest.getEmail());
         userResponse.setPhone(userRequest.getPhone());
         return userResponse;
     }
-//    @DeleteMapping(value = "/deleteUser")
-//    public void deleteUser(Integer userId){
-//
-//    }
 }
---
+++ src/main/java/com/example/user/services/UserService.java
@@ -0,0 +1,7 @@
+package com.example.user.services;
+
+import com.example.user.model.user.dto.User;
+
+public interface UserService {
+    User createUser(User user);
+}
